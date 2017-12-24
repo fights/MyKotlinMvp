0b9996511ea94d274f01ca2093799416c396f1b3
Mr zhang <fight__fight@163.com>
2017-12-24 22:39:42 +0800

    [ADD] test feature on feature-1 branch.

--- app/src/main/java/com/android/mykotlinmvp/MainActivity.kt
+++ app/src/main/java/com/android/mykotlinmvp/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //添加log, 验证分支提交
     }
 }
