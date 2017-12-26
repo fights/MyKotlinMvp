c0e0764f1a46516bb500c9aae2c300e850295bdf
zhangguanjun <zhangguanjun@sinovoice.com.cn>
2017-12-26 19:08:06 +0800

    [ADD] 拷贝图片资源。

---
+++ app/src/main/java/com/android/mykotlinmvp/ui/activiy/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.android.mykotlinmvp.ui.activiy
+
+import com.android.mykotlinmvp.R
+import com.android.mykotlinmvp.ui.base.BaseActivity
+
+/**
+ * Created by zhangguanjun on 2017/12/26.
+ */
+class SplashActivity : BaseActivity() {
+
+
+    override fun init() {
+    }
+
+    override fun getLayoutId(): Int = R.layout.activity_splash
+
+    override fun initView() {
+    }
+
+    override fun getData() {
+    }
+
+}
