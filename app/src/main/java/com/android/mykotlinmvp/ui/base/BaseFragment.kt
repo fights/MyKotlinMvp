32fa9300a5537c973a9571e4264afd2783492698
zhangguanjun <zhangguanjun@sinovoice.com.cn>
2017-12-29 17:40:38 +0800

    [ADD] 编写BaseFragment。

---
+++ app/src/main/java/com/android/mykotlinmvp/ui/base/BaseFragment.kt
@@ -0,0 +1,58 @@
+package com.android.mykotlinmvp.ui.base
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.multiple_state_view.MultipleStatusView
+
+/**
+ * Created by zhangguanjun on 2017/12/29.
+ */
+abstract class BaseFragment: Fragment(){
+    protected var mRootView: View? = null
+    private var mIsViewCreated = false
+    private var mIsVisibleToUser = false
+    private var mHasLoadData = false
+
+    protected var mMutipleStatusView: MultipleStatusView? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        mRootView = inflater?.inflate(getLayoutId(),null)
+        return mRootView!!
+    }
+
+    abstract fun getLayoutId(): Int
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initListener()
+        mMutipleStatusView?.setOnRetryClickListener(mRetryClickListener)
+        mIsViewCreated = true
+        lazyLoadDataIfPrepared()
+    }
+
+    open var mRetryClickListener = View.OnClickListener { loadData() }
+
+    private fun lazyLoadDataIfPrepared() {
+        if (mIsVisibleToUser && !mHasLoadData && mIsVisibleToUser) {
+            loadData()
+            mHasLoadData = true
+        }
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (isVisibleToUser) {
+            lazyLoadDataIfPrepared()
+        }
+    }
+
+    abstract fun loadData()
+
+    abstract fun initListener()
+
+    abstract fun initView()
+}
