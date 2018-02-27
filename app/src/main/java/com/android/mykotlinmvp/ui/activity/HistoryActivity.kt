3ddfc06dc7b79299ee9913295a022ad75ba2d1d7
zhangguanjun <zhangguanjun@sinovoice.com.cn>
2018-02-27 11:00:30 +0800

    [ADD] 简单编写我的界面。

---
+++ app/src/main/java/com/android/mykotlinmvp/ui/activity/HistoryActivity.kt
@@ -0,0 +1,21 @@
+package com.android.mykotlinmvp.ui.activity
+
+import com.android.mykotlinmvp.R
+import com.android.mykotlinmvp.ui.base.BaseActivity
+
+/**
+ * Created by zhangguanjun on 2018/2/27.
+ */
+class HistoryActivity : BaseActivity() {
+    override fun init() {
+    }
+
+    override fun getLayoutId(): Int = R.layout.activity_history
+
+    override fun initView() {
+    }
+
+    override fun getData() {
+    }
+
+}
--- app/src/main/java/com/android/mykotlinmvp/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/android/mykotlinmvp/ui/fragment/MineFragment.kt
@@ -1,8 +1,12 @@
 package com.android.mykotlinmvp.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import com.android.mykotlinmvp.R
+import com.android.mykotlinmvp.ui.activity.HistoryActivity
 import com.android.mykotlinmvp.ui.base.BaseFragment
+import com.android.mykotlinmvp.view.glide.GlideApp
+import kotlinx.android.synthetic.main.fragment_mine.*
 
 /**
  * Created by zhangguanjun on 2018/1/4.
@@ -26,9 +30,17 @@ class MineFragment : BaseFragment() {
     }
 
     override fun initListener() {
+        tv_fm_history.setOnClickListener {
+            //跳转到观看历史记录界面
+            startActivity(Intent(activity, HistoryActivity::class.java))
+        }
     }
 
     override fun initView() {
+        GlideApp.with(this)
+                .load(R.mipmap.default_avatar)
+                .circleCrop()
+                .into(iv_fm_header)
     }
 
 }
